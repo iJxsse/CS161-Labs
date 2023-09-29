11b1f20ffd017bbacdb65db2eb3806b19f00fafb
iJxsse <123838537+iJxsse@users.noreply.github.com>
2023-09-29 20:12:36 +0100

    Update Study.java

--- Lab 00/Study.java
+++ Lab 00/Study.java
@@ -1,1 +1,11 @@
+// Writing a Java program which prints the message "I am studying Computer Science at Maynooth University" exactly to the screen.
+
+public class Study {
+    
+    public static void main (String [] args) {
+        // Printing text to the screen
+        System.out.println("I am studying Computer Science at Maynooth University");
+    }
+
+}
 
