32d032723d69ad784681bcfe35428a14bff8ecae
iJxsse <123838537+iJxsse@users.noreply.github.com>
2023-09-29 20:07:14 +0100

    Create Printing.java
    Intro Task - Lab 00

---
+++ Printing.java
@@ -0,0 +1,9 @@
+// Writing a java program which prints the message "Welcome to CS161" exactly to the screen.
+
+public class Printing {
+
+    public static void main (String [] args) {
+        // Printing text to the screen
+        System.out.println("Welcome to CS161");
+    }
+}
