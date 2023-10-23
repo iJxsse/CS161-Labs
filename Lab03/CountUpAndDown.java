f2c4ef5ade5a1c56be4197c9cf4d117c85965237
Jesse <123838537+iJxsse@users.noreply.github.com>
2023-10-23 23:42:02 +0100

    Exercise 1

---
+++ Lab03/CountUpAndDown.java
@@ -0,0 +1,16 @@
+public class UpDown {
+
+  public static void main(String[] args) {
+
+    int i = 10;
+
+    while (i <= 10) {
+      System.out.print(i + " ");
+      i++;
+    }
+    System.out.println();
+    for (i = 20; i >= 10; i--) {
+      System.out.print(i + " ");
+    }
+  }
+}
