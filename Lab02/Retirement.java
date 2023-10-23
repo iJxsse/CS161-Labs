c533e164489c1c4e2610e76e5df048cb25bcf6e7
Jesse <123838537+iJxsse@users.noreply.github.com>
2023-10-23 21:54:50 +0100

    Exercise 1

---
+++ Lab02/Retirement.java
@@ -0,0 +1,16 @@
+// Java program that checks to see if the age of a person is greater than or equal to 66
+// If it is, prints out that the person is retired; otherwise, print that they are not retired
+
+public class Retirement {
+
+  public static void main (String[] args) {
+
+    int age = 65;
+
+    if (age >= 66) {
+      System.out.println("Person is retired");
+    } else {
+      System.out.println("Person is not retired");
+    } 
+  }
+}
